861a01239208ad2e6606d85c4685582dc8b50365
salmansaleem2 <salmansaleem8523@gmail.com>
2023-06-01 14:22:42 +0500

    Creating Routes & Responses In Express

--- server.js
+++ server.js
@@ -7,6 +7,36 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  // for text/html
+  // res.send("<h1>Hello from express</h1>");
+  // for JSON data
+  // res.send({ name: "Hello from express" });
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `show bootcamps ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamps" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamps ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamps ${req.params.id}` });
+});
+
 app.listen(PORT, () => {
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
