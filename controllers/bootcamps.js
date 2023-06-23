cc6926c8688d1ba535b6d11fa182a57882ed058e
salmansaleem2 <salmansaleem8523@gmail.com>
2023-06-23 12:13:11 +0500

    select and sorting in progress

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,14 +8,39 @@ const geocoder = require("../utils/geocoder");
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ["select"];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  console.log(reqQuery);
+
+  // Create query string
   let queryStr = JSON.stringify(req.query);
+
+  // Create operators ($gt.$gte,etc)
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
 
+  // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    console.log(fields);
+    console.log(query);
+    query = query.select(fields);
+  }
+
+  // Executing query
   const bootcamps = await query; // this bootcamp is model
   res
     .status(200)
