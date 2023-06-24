7da41672840974205719ff06b0a8340711ad592b
salmansaleem2 <salmansaleem8523@gmail.com>
2023-06-24 14:07:56 +0500

    feat():added select and sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,15 +13,13 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ["select"];
+  const removeFields = ["select", "sort"];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param]);
 
-  console.log(reqQuery);
-
   // Create query string
-  let queryStr = JSON.stringify(req.query);
+  let queryStr = JSON.stringify(reqQuery);
 
   // Create operators ($gt.$gte,etc)
   queryStr = queryStr.replace(
@@ -35,9 +33,15 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   // Select Fields
   if (req.query.select) {
     const fields = req.query.select.split(",").join(" ");
-    console.log(fields);
-    console.log(query);
     query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
   }
 
   // Executing query
