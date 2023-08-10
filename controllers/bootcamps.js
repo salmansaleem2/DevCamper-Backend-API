17602eeeb94fd6a7aafd7f472e387502a0868e4b
salmansaleem2 <salmansaleem8523@gmail.com>
2023-08-10 13:26:43 +0500

    feat:added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -57,6 +57,12 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route POST /api/v1/bootcamps
 // @access Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req,body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({});
+
   const bootcamp = await Bootcamp.create(req?.body);
 
   res.status(201).json({
