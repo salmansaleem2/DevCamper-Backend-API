c95dfa0271c4bd6d8190ad26abfb9b4ca356634e
salmansaleem2 <salmansaleem8523@gmail.com>
2023-08-26 15:01:36 +0500

    feat: logout to clear token cookie

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
   resetPassword,
   updateDetails,
   updatePassword,
+  logout,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -16,6 +17,7 @@ const { protect } = require("../middlewear/auth");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.get("/logout", logout);
 router.put("/updateDetails", protect, updateDetails);
 router.put("/updatePassword", protect, updatePassword);
 router.post("/forgotPassword", forgotPassword);
